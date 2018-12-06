c80d146f966488bf8040e13a0312c0881d69f8dc
ayadandal <ayadandal19@gmail.com>
2018-12-06 11:32:20 +0200

    firs commit

---
+++ HW1.py
@@ -0,0 +1,7 @@
+m1,m2,s1,n1 = "aaa","yyy",5,0.5
+vars = [m1 ,m2,s1,n1]
+print ("the type of list (vars) is: " +str(type(vars))+"\n" +"type of the first element is:"+str(type(vars[0]))+"\n" +"type of the second element is:"+str(type(vars[1]))+"\n" +"type of the third element is:"+str(type(vars[2]))+"\n" +"type of the fourth element is:"+str(type(vars[3])) )
+print("the first element is: " +vars[0] +"\nthe last element is: " +str(vars[-1]) )
+vars[8] = 12
+print (vars[8])
+input("press enter to exit")
